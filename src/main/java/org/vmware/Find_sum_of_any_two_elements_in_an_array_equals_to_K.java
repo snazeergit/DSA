96a6688fa5555aa93db5c77446100bbbbc284357
Syed Nazeer <nazeersd.java@gmail.com>
2025-08-09 22:13:19 +0530

    Two Pointers POC
    Signed-off-by: Syed Nazeer <nazeersd.java@gmail.com>

--- src/main/java/org/vmware/Find_sum_of_any_two_elements_in_an_array_equals_to_K.java
+++ src/main/java/org/vmware/Find_sum_of_any_two_elements_in_an_array_equals_to_K.java
@@ -7,8 +7,7 @@ public class Find_sum_of_any_two_elements_in_an_array_equals_to_K {
     public static void main(String[] args) {
 
         int[] ar = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        int n = ar.length;
-        int l = 0, r = n - 1, k = 17;
+        int l = 0, r = ar.length - 1, k = 17;
         boolean flag=false;
         while (l < r) {
             if (ar[l] + ar[r] == k) {
