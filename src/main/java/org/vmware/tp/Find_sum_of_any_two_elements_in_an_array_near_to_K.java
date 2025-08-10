910597233554ff68c8e4f6f0375d6ec547353819
Syed Nazeer <nazeersd.java@gmail.com>
2025-08-10 11:46:59 +0530

    Two Pointers POC
    Signed-off-by: Syed Nazeer <nazeersd.java@gmail.com>

--- src/main/java/org/vmware/tp/Find_sum_of_any_two_elements_in_an_array_near_to_K.java
+++ src/main/java/org/vmware/tp/Find_sum_of_any_two_elements_in_an_array_near_to_K.java
@@ -1,5 +1,7 @@
 package org.vmware.tp;
 
+import java.util.Arrays;
+
 /*
  * Find sum of any two numbers close to K
  */
@@ -7,22 +9,21 @@ public class Find_sum_of_any_two_elements_in_an_array_near_to_K {
     public static void main(String[] args) {
 
         int[] ar = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        int l = 0, r = ar.length - 1, k = 21, sum = 0, dif = 0;
-        int closestL = -1, closestR = -1, closestDif = Integer.MAX_VALUE;
+        int l = 0, r = ar.length - 1, k = 7;
+        int closestL = l, closestR = r, closestDif = Arrays.stream(ar).max().getAsInt();
 
         while (l < r) {
-            sum = ar[l] + ar[r];
-            dif = Math.abs(sum - k);
+            int sum = ar[l] + ar[r];
+            int dif = Math.abs(sum - k);
             if (sum == k) {
                 System.out.println("Exact match found :" + ar[l] + ", " + ar[r]);
-                break;
+                return;
             }
             if (dif < closestDif) {
                 closestDif = dif;
                 closestL = l;
                 closestR = r;
-            }
-            if (sum > k) {
+            } else if (sum > k) {
                 r--;
             } else {
                 l++;
